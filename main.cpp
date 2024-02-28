91a4fc1a7e9a4a72f51b4b194a0ccf1d5d5a8726
Ruddyard_Castro <rcastroc2@miumg.edu.gt>
2024-02-28 10:16:30 -0600

    Agregar cliclos electorales y cambar valres al imprirmr la tabla

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ using namespace std;
 
 #define numero_Candidatos 5
 #define yearElectorales 4
-#define NUMERO_NOTAS 4
+#define numeroCiclos 5
 #define MAX_CALIFICACION 1000000
 #define MIN_CALIFICACION 0
 #define MAXIMA_LONGITUD_CADENA 100
 
 void llamaCiclo();
 int busquedaAleatorios(int minimo, int maximo);
-void llenarMatriz(float matriz[numero_Candidatos][NUMERO_NOTAS + 1]);
+void llenarMatriz(float matriz[numero_Candidatos][numeroCiclos + 1]);
 void imprimirMatrizLinea();
-float imprimirMatriz(float matriz[numero_Candidatos][NUMERO_NOTAS + 1], char alumnos[numero_Candidatos][MAXIMA_LONGITUD_CADENA], string nombrecicloElectoral);
+float imprimirMatriz(float matriz[numero_Candidatos][numeroCiclos + 1], char Candidatos[numero_Candidatos][MAXIMA_LONGITUD_CADENA], string nombrecicloElectoral);
 int main()
 {
     srand(getpid());
@@ -28,30 +28,36 @@ int main()
 }
 void llamaCiclo()
 {
-    float matriz_cicloElectoral_1[numero_Candidatos][NUMERO_NOTAS + 1];
-    float matriz_cicloElectoral_2[numero_Candidatos][NUMERO_NOTAS + 1];
-    float matriz_cicloElectoral_3[numero_Candidatos][NUMERO_NOTAS + 1];
+    float matriz_cicloElectoral_1[numero_Candidatos][numeroCiclos + 1];
+    float matriz_cicloElectoral_2[numero_Candidatos][numeroCiclos + 1];
+    float matriz_cicloElectoral_3[numero_Candidatos][numeroCiclos + 1];
+        float matriz_cicloElectoral_4[numero_Candidatos][numeroCiclos + 1];
     char opcion;
     bool repetir = true;
 
     float promedio_cicloElectoral_1;
     float promedio_cicloElectoral_2;
     float promedio_cicloElectoral_3;
-
-    char alumnos[numero_Candidatos][MAXIMA_LONGITUD_CADENA] = {"Carlos","Luis","Maria","Pedro","Juan"};
+    float promedio_cicloElectoral_4;
+    char Candidatos[numero_Candidatos][MAXIMA_LONGITUD_CADENA] = {"Carlos","Luis","Maria","Pedro","Juan"};
     do
     {
         system("cls");
         promedio_cicloElectoral_1=0;
         promedio_cicloElectoral_2=0;
         promedio_cicloElectoral_3=0;
+        promedio_cicloElectoral_4=0;
         cout << "*** Comparativo de cicloElectorales ***" << endl << endl;
         llenarMatriz(matriz_cicloElectoral_1);
-        promedio_cicloElectoral_1 = imprimirMatriz(matriz_cicloElectoral_1, alumnos, "cicloElectoral de 2017");
+        promedio_cicloElectoral_1 = imprimirMatriz(matriz_cicloElectoral_1, Candidatos, "cicloElectoral de 2017");
         llenarMatriz(matriz_cicloElectoral_2);
-        promedio_cicloElectoral_2 = imprimirMatriz(matriz_cicloElectoral_2, alumnos, "cicloElectoral de 2019");
+        promedio_cicloElectoral_2 = imprimirMatriz(matriz_cicloElectoral_2, Candidatos, "cicloElectoral de 2019");
         llenarMatriz(matriz_cicloElectoral_3);
-        promedio_cicloElectoral_3 = imprimirMatriz(matriz_cicloElectoral_3, alumnos, "cicloElectoral de 2019");
+        promedio_cicloElectoral_3 = imprimirMatriz(matriz_cicloElectoral_3, Candidatos, "cicloElectoral de 2019");
+        llenarMatriz(matriz_cicloElectoral_4);
+        promedio_cicloElectoral_4 = imprimirMatriz(matriz_cicloElectoral_4, Candidatos, "cicloElectoral de 2023");
+
+
         if (promedio_cicloElectoral_1 > promedio_cicloElectoral_2 && promedio_cicloElectoral_1 > promedio_cicloElectoral_3)
         {
             cout << " La cicloElectoral con el mejor promedio es : " << "cicloElectoral de 2017" << " Promedio: " << promedio_cicloElectoral_1 << endl;
@@ -80,19 +86,19 @@ int busquedaAleatorios(int minimo, int maximo)
     return minimo + rand() / (RAND_MAX / (maximo - minimo + 1) + 1);
 }
 
-void llenarMatriz(float matriz[numero_Candidatos][NUMERO_NOTAS + 1])
+void llenarMatriz(float matriz[numero_Candidatos][numeroCiclos + 1])
 {
     int y, x;
     for (y = 0; y < numero_Candidatos; y++)
     {
         float suma = 0;
         int calificacion = 0;
-        for (x = 0; x < NUMERO_NOTAS; x++)
+        for (x = 0; x < numeroCiclos; x++)
         {
-            if (x == 0 || x == 3)  //primer parcial
+            if (x == 0 || x == 3)  //primer cicloelectoral
             {
                 calificacion = busquedaAleatorios(MIN_CALIFICACION, 20);
-            } else if (x == 1)  //segundo parcial
+            } else if (x == 1)  //segundo cicloelectoral
             {
                 calificacion = busquedaAleatorios(MIN_CALIFICACION, 25);
             } else if (x == 2)  //examen final
@@ -105,7 +111,7 @@ void llenarMatriz(float matriz[numero_Candidatos][NUMERO_NOTAS + 1])
         }
         // Agregar promedio
 
-        matriz[y][NUMERO_NOTAS] = suma;
+        matriz[y][numeroCiclos] = suma;
     }
 }
 
@@ -114,55 +120,55 @@ void imprimirMatrizLinea()
     int x;
 
     cout << "+--------";
-    for (x = 0; x < NUMERO_NOTAS + 1; x++)
+    for (x = 0; x < numeroCiclos + 1; x++)
     {
         cout << "+---------";
     }
     cout << "+\n";
 }
-float imprimirMatriz(float matriz[numero_Candidatos][NUMERO_NOTAS + 1], char alumnos[numero_Candidatos][MAXIMA_LONGITUD_CADENA], string nombrecicloElectoral)
+float imprimirMatriz(float matriz[numero_Candidatos][numeroCiclos + 1], char Candidatos[numero_Candidatos][MAXIMA_LONGITUD_CADENA], string nombrecicloElectoral)
 {
     //Funci�n que imprime la matriz en pantalla y realizando los calculos necesarios del promedio
     int y, x;
 
-    float promedioMayor = matriz[0][NUMERO_NOTAS];
-    float promedioMenor = matriz[0][NUMERO_NOTAS];
+    float promedioMayor = matriz[0][numeroCiclos];
+    float promedioMenor = matriz[0][numeroCiclos];
     float totalGeneral = 0;
     float promedioGeneral = 0;
     char mayor[MAXIMA_LONGITUD_CADENA];
     char promMenor[MAXIMA_LONGITUD_CADENA];
-    memcpy(mayor, alumnos[0], MAXIMA_LONGITUD_CADENA);
-    memcpy(promMenor, alumnos[0], MAXIMA_LONGITUD_CADENA);
+    memcpy(mayor, Candidatos[0], MAXIMA_LONGITUD_CADENA);
+    memcpy(promMenor, Candidatos[0], MAXIMA_LONGITUD_CADENA);
     cout << nombrecicloElectoral << endl;
-    cout << "(Nota1)=>Primer Parcial  (Nota2)=>Segundo Parcial (Nota3)=>Final (Nota4)=>Actividades" << endl;
+    cout << "(PrimerCiclo)=>Primer cicloelectoral  (SegundoCiclo)=>Segundo cicloelectoral (Tercerciclo)=>Final (CuartoCiclo)=>Actividades" << endl;
     imprimirMatrizLinea();
-    cout << setw(9) << "Alumno";
-    for (x = 0; x < NUMERO_NOTAS; x++)
+    cout << setw(9) << "Candidato";
+    for (x = 0; x < numeroCiclos; x++)
     {
-        cout << setw(9) << "Nota" << x + 1;
+        cout << setw(9) << "Votos " << x + 1;
     }
-    cout << setw(8) << "Tot" << endl;
+    cout << setw(8) << "Tot " << endl;
     imprimirMatrizLinea();
     for (y = 0; y < numero_Candidatos; y++)
     {
-        cout << "!" << setw(8) << alumnos[y] << "!";
+        cout << "!" << setw(8) << Candidatos[y] << "!";
         float suma = 0;
-        for (x = 0; x < NUMERO_NOTAS; x++)
+        for (x = 0; x < numeroCiclos; x++)
         {
             int calificacion = matriz[y][x];
             cout << setw(9) << calificacion << "!";
         }
-        float promedio = matriz[y][NUMERO_NOTAS];
-        totalGeneral += matriz[y][NUMERO_NOTAS];
+        float promedio = matriz[y][numeroCiclos];
+        totalGeneral += matriz[y][numeroCiclos];
         if (promedio > promedioMayor)
         {
             promedioMayor = promedio;
-            memcpy(mayor, alumnos[y], MAXIMA_LONGITUD_CADENA);
+            memcpy(mayor, Candidatos[y], MAXIMA_LONGITUD_CADENA);
         }
         if (promedio < promedioMenor)
         {
             promedioMenor = promedio;
-            memcpy(promMenor, alumnos[y], MAXIMA_LONGITUD_CADENA);
+            memcpy(promMenor, Candidatos[y], MAXIMA_LONGITUD_CADENA);
         }
         cout << setw(9) << fixed << setprecision(2) << promedio << "!" << endl;
         imprimirMatrizLinea();
